7e36ff7c9b9814c70385c5e6ca68fca8f61408b0
Your Name <M0H1K4>
2023-02-01 16:03:40 +0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import icon from './icon-music.svg';
+import ilustration from './illustration-hero.svg';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Order Summary</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          You can now listen to millions of songs,
+           audiobooks, and podcasts on any 
+           device anywhere you like!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='annual'>
+          <img className='icon' src={icon} alt="" />
+          <h4 className='plan'>Annual Plan</h4>
+          <h4 className='year'>$59.99/year</h4>
+          <h4 className='change'>Change</h4>
+        </div>
+        <div>
+          <button className='proceed'>Proceed to Payment</button>
+          <br />
+          <h3 className="cancel">Cancel Order</h3>
+        </div>
     </div>
   );
 }
